55fd29111c4383fd07897242f6c8ecb00955231a
Carlos Sosa <cjsq24@gmail.com>
2022-10-24 20:04:22 -0400

    react router v6

--- src/layouts/adminPanel/AdminPanel.tsx
+++ src/layouts/adminPanel/AdminPanel.tsx
@@ -1,21 +1,26 @@
-import React, { FC, useState } from 'react';
+/* import React, { useState } from 'react';
 import { Layout } from 'antd';
 import '../../assets/css/adminPanelStyles.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 
-const { Content } = Layout;
+const { Content } = Layout; */
 
-const AdminPanel: FC = ({ children }) => {
-  const [collapsed, setCollapsed] = useState<boolean>(false);
+/* type TProps = {
+  children: JSX.Element;
+}; */
+
+const AdminPanel = (/* { children }: TProps */) => {
+  /* const [collapsed, setCollapsed] = useState<boolean>(false);
 
   const toggle = (): void => {
     setCollapsed(!collapsed);
-  };
+  }; */
 
   return (
-    <Layout style={{ height: '100vh' }}>
+    <>
+      {/* <Layout style={{ height: '100vh' }}>
       <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
       <Layout className="site-layout">
         <Header collapsed={collapsed} toggle={toggle} />
@@ -31,7 +36,8 @@ const AdminPanel: FC = ({ children }) => {
         </Content>
         <Footer />
       </Layout>
-    </Layout>
+    </Layout> */}
+    </>
   );
 };
 
--- src/views/landingPage/contactMe/ContactMe.tsx
+++ src/views/landingPage/contactMe/ContactMe.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import HeaderTitle from '../components/HeaderTitle';
 
 const ContactMe = () => {
